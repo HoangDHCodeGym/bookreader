2719d255a696fa792f3caebcc0bb084ffcaa529b
HoangDHCodeGym <ol.hoangdh.thna@gmail.com>
2018-12-03 01:51:15 +0700

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ReadingListComponent } from './reading-list/reading-list.component';
+import { ArchiveComponent } from './archive/archive.component';
+import {AppRoutingModule} from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReadingListComponent,
+    ArchiveComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
